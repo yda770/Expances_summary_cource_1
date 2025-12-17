b101f9b26b9ca1403a28f6d8abcd7626cc139580
z-yehuda-ds <yehudad2@rail.co.il>
2025-12-17 12:37:29 +0200

    feat: Implement expense tracking functionality with components for adding and displaying expenses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+
+import './Expenses.css';
+import Expenses from "./components/Expenses/Expenses.js";
+import expensesData from "./components/Expenses/ExpensesData.js";
+import NewExpense from "./components/NewExpense/NewExpense.js";
+
+
 function App() {
+  const [expenses, setExpenses] = useState(expensesData);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate.js';
+import Card from '../UI/Card.js';
+
+function ExpenseItem({ date, title, amount }) {
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={date} />
+
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount.toFixed(2)}</div>
+            </div>
+        </Card>
+    );
+}
+export default ExpenseItem;
